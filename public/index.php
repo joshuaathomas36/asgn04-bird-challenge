501ae73046d65bb6df0cc06ee8cb4494a0cedd00
Joshua Thomas <joshuaathomas36@students.abtech.edu>
2021-09-25 17:51:00 -0400

    index.php complete, moving to bird.php

--- public/index.php
+++ public/index.php
@@ -5,12 +5,12 @@
 <div id="main">
 
   <ul id="menu">
-    <li><a href="<?php echo url_for('/bicycles.php'); ?>">View Our Inventory</a></li>
+    <li><a href="<?php echo url_for('/bird.php'); ?>">View Our Birds</a></li>
     <li><a href="<?php echo url_for('/about.php'); ?>">About Us</a></li>
   </ul>
     
 </div>
 
-<?php $super_hero_image = 'tufted-titmouse.jpg'; ?>
+<?php $super_hero_image = ''; ?>
 
 <?php include(SHARED_PATH . '/public_footer.php'); ?>
